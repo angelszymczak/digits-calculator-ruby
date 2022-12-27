c7fb4e7c1d500c5481a95112c1efac6ffc0fad8a
Angel Szymczak <juanangelszymczak@gmail.com>
2022-12-27 01:26:30 -0300

    test: 🔴 should handle invalid arguments

--- spec/digits_calculator_spec.rb
+++ spec/digits_calculator_spec.rb
@@ -5,21 +5,39 @@ require_relative '../lib/digits_calculator'
 RSpec.describe DigitsCalculator do 
   subject { described_class.call(parameter) }
 
-  context 'when receiving 16' do
+  context 'when receiving "16"' do
     let(:parameter) { '16' }
 
     it { is_expected.to eq(7) }
   end 
 
-  context 'when receiving 942' do
+  context 'when receiving "942"' do
     let(:parameter) { '942' }
 
     it { is_expected.to eq(6) }
   end 
 
-  context 'when receiving 9999999' do
+  context 'when receiving "9999999"' do
     let(:parameter) { '9999999' }
 
     it { is_expected.to eq(9) }
   end 
+
+  context 'when receiving a non-string parameter' do
+    let(:parameter) { 16 }
+
+    it do
+      expect { subject }
+        .to raise_error(described_class::NonStringArgumentError, 'Non-String argument is not allowed')
+    end
+  end
+
+  context 'when receiving a non-numeric string parameter' do
+    let(:parameter) { 'non-numeric' }
+
+    xit do
+      expect { subject }
+        .to raise_error(described_class::NonNumericArgumentError, 'Non-Numeric String argument is not allowed')
+    end
+  end
 end
