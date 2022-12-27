c174b879b6a79a14bfe08826048cba8d8f5722b8
Angel Szymczak <juanangelszymczak@gmail.com>
2022-12-27 00:42:36 -0300

    feat:[wip] 🔴 scale solution for 16 input

--- lib/digits_calculator.rb
+++ lib/digits_calculator.rb
@@ -6,9 +6,18 @@ class DigitsCalculator
   # @returns int
   def self.call(number)
     case number
-    when '16' then 7
+    when '16' then calculate(number)
     when '942' then 6
     else nil
     end
   end
+
+  def self.calculate(number)
+    number
+      .split('')
+      .map(&:to_i)
+      .sum
+  end
+
+  private_class_method :calculate
 end
